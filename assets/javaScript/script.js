cdb8fb880a357912e761958a6c57f8f7f0b64dbc
kbetlach <kbetlach@gmail.com>
2020-05-05 13:50:16 -0500

    tweaked media queries, though theyre not perfect. api integration and format is finished

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,6 +1,6 @@
-function instagramAPI() {
+function callInstagramAPI() {
 
-    var queryURL = "https://www.instagram.com/goblue42agency/?__a=1";
+    let queryURL = "https://www.instagram.com/goblue42agency/?__a=1";
 
     console.log(queryURL);
 
@@ -10,22 +10,20 @@ function instagramAPI() {
         console.log(queryURL);
 
         $(".profile-pic").attr('src', response.graphql.user.profile_pic_url);
-        $(".name").html(response.graphql.user.full_name);
-        $(".biography").html(response.graphql.user.biography);
-        $(".username").html(response.graphql.user.username);
-        $(".number-of-posts").html(response.graphql.user.edge_owner_to_timeline_media.count);
-        $(".followers").html(response.graphql.user.edge_followed_by.count);
-        $(".following").html(response.graphql.user.edge_follow.count);
+        $("#biography").html(response.graphql.user.biography);
+        $("#number-of-posts").html(response.graphql.user.edge_owner_to_timeline_media.count + " posts");
+        $("#followers").html(response.graphql.user.edge_followed_by.count + " followers");
+        $("#following").html(response.graphql.user.edge_follow.count + " following");
         posts = response.graphql.user.edge_owner_to_timeline_media.edges;
         posts_html = '';
         for (var i = 0; i < posts.length; i++) {
             url = posts[i].node.display_url;
             likes = posts[i].node.edge_liked_by.count;
             comments = posts[i].node.edge_media_to_comment.count;
-            posts_html += '<div class="col-md-4 equal-height"><img style="min-height:50px;background-color:#fff;width:100%" src="' + url + '"><div class="row like-comment"><div class="col-md-6">' + (likes) + ' LIKES</div><div class="col-md-6">' + (comments) + ' COMMENTS</div></div></div>';
+            posts_html += '<div class="col-md-4 equal-height text-center"><img style="border:1px solid #F0AD4E;padding:5px;height:293px;width:293px;background-color:#fff;width:100%" src="' + url + '"><div class="row like-comment"><div class="col-md-6">' + (likes) + ' LIKES</div><div class="col-md-6">' + (comments) + ' COMMENTS</div></div></div>';
         }
-        $(".posts").html(posts_html);
+        $("#posts").html(posts_html);
     });
 };
 
-instagramAPI();
+callInstagramAPI();
